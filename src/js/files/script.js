252699ea24d9680adef7f2fad2d310c734e7e7a9
Andrey Stseba <scebaandru@gmail.com>
2022-05-12 21:55:01 +0300

    adding link menu active class

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,13 @@
 // Подключение функционала "Чертогов Фрилансера"
-import { isMobile } from "./functions.js";
+import { isMobile } from './functions.js'
 // Подключение списка активных модулей
-import { flsModules } from "./modules.js";
+import { flsModules } from './modules.js'
+
+let pageId = document
+    .querySelector('[data-id-page]')
+    .getAttribute('data-id-page'),
+  navItem = document.querySelector(`[data-id-nav=${pageId}]`)
+
+if (pageId == navItem.getAttribute('data-id-nav')) {
+  navItem.classList.add('_active')
+}
