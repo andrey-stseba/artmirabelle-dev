292846aff2adaca075a1ab70b41437f376f0117c
andrey <scebaandru@gmail.com>
2022-05-29 21:23:58 +0300

    creating team page

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -60,9 +60,9 @@ const config = {
     // Расскоментировать на слабом ПК
     // (в режиме разработчика, папка с результаттом будет создаваться на диске)
 
-    devMiddleware: {
-      writeToDisk: true
-    },
+    // devMiddleware: {
+    //   writeToDisk: true
+    // },
 
     watchFiles: [
       `${paths.src}/**/*.html`,
